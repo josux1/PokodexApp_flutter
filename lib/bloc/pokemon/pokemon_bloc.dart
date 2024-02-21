9e038989c9027ea06ba95708fc7c40888d17d23f
josux1 <micorreoonline6@gmail.com>
2024-02-21 17:03:59 -0600

    BuildFix

--- lib/bloc/pokemon/pokemon_bloc.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pokodex/models/pokemon_model.dart';
-
-part 'pokemon_event.dart';
-part 'pokemon_state.dart';
-
-class PokemonBloc {}
--- lib/bloc/pokemon/pokemon_event.dart
+++
@@ -1,3 +0,0 @@
-part of 'pokemon_bloc.dart';
-
-abstract class PokemonEvent {}
